3fca53f963e57d0339613f02b0cc12949160694e
Lars Passic <larspassic@outlook.com>
2020-11-07 09:23:30 -0800

    Uploading project
    Copied files in to the correct folder.

---
+++ Challenges/WrittenNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WrittenNumbers
+{
+    class Program
+    {
+        /// <summary>
+        /// Write a program that, given an amount prints the appropriate text. 
+        /// For example:
+        /// 4580.34 would print "4 Thousand 5 hundred 80 Dollars and 34 cents"
+        /// </summary>
+        /// <param name="args"></param>
+        /// 
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
