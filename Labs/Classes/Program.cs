27c1e18c05ea48e9e931e36c12fb645a9de3c65d
Lars Passic <larspassic@outlook.com>
2020-11-09 20:52:31 -0800

    Created lab "Classes"

---
+++ Labs/Classes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+// TODO: Define the Contact class to make this compile
+
+class Program
+{
+    static void Main()
+    {
+        Contact contact = new Contact();
+
+        contact.FirstName = "FirstName";
+        contact.LastName = "LastName";
+        contact.Age = 23;
+
+        // GetFullName() returns "FirstName LastName"
+        Console.WriteLine("Name: {0} Age: {1}", contact.GetFullName(), contact.Age);
+    }
+}
