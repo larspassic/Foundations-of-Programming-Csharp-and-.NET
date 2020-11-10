c32c07d869d56bc4d147f4cf265548c30d50ff2a
Lars Passic <larspassic@outlook.com>
2020-11-09 21:32:50 -0800

    Finished with lab Classes

--- Labs/Classes/Program.cs
+++ Labs/Classes/Program.cs
@@ -2,17 +2,30 @@
 
 // TODO: Define the Contact class to make this compile
 
+class Contact
+{
+    public string FirstName;
+    public string LastName;
+    public int Age;
+
+    public String GetFullName()
+    {
+        return ($"{FirstName} {LastName}");
+    }
+}
+
 class Program
 {
     static void Main()
     {
         Contact contact = new Contact();
 
-        contact.FirstName = "FirstName";
-        contact.LastName = "LastName";
-        contact.Age = 23;
+        contact.FirstName = "Lars";
+        contact.LastName = "Passic";
+        contact.Age = 32;
 
         // GetFullName() returns "FirstName LastName"
         Console.WriteLine("Name: {0} Age: {1}", contact.GetFullName(), contact.Age);
+        Console.ReadLine();
     }
 }
