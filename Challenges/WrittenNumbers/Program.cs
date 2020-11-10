943e06824da030efd0ea03ab54c1d304d423901c
Lars Passic <larspassic@outlook.com>
2020-11-10 09:35:20 -0800

    Fiding output to the user portion

--- Challenges/WrittenNumbers/Program.cs
+++ Challenges/WrittenNumbers/Program.cs
@@ -73,24 +73,33 @@ namespace WrittenNumbers
                 //Write output to the user
                 string finalString = "";
 
+                //Don't write thousands if there aren't any
                 if (thousands > 0)
                 {
                     finalString = $"{thousands} thousand ";
                 }
+                //Don't write hundreds if there aren't any
                 if (hundreds > 0)
                 {
                     finalString = finalString + ($"{hundreds} hundred ");
                 }
+                //Don't write tens if there aren't any
                 if (tens > 0)
                 {
                     finalString = finalString + ($"{tens} ");
                 }
+                //Only write the word dollars if any of the three dollar results are at least greater than zero
                 if (thousands > 0 || hundreds > 0 || tens > 0)
                 {
                     finalString = finalString + "dollars ";
                 }
-
-                finalString = finalString + ($"and {cents} cents.");
+                //If there has been nothing thus far, no need to write and
+                if (finalString != "")
+                {
+                    finalString = finalString + ($"and ");
+                }
+                //Always write cents even if zero, just like on a check
+                finalString = finalString + ($"{cents} cents.");
                 Console.WriteLine(finalString);
                 Console.WriteLine();
 
