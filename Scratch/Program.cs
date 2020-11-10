00725c9c103f50a9df9f888261a483c454086a33
Lars Passic <larspassic@outlook.com>
2020-11-09 20:23:57 -0800

    Updating scratch pad
    Break in the lesson.

--- Scratch/Program.cs
+++ Scratch/Program.cs
@@ -1,22 +1,38 @@
 ﻿// File: VerySimple.cs
 using System;
 
-class VerySimple
-{
-    int simpleValue;
 
-    public VerySimple()
+
+//Back-end architect developers who create tools and classes for us to use
+class VerySimple //This is a class
+{
+    public int simpleValue; //Field - added the "public" to make it available in the test below
+
+    public VerySimple() //Constructor
     {
-        simpleValue = 10;
+        //initializers : superintendents of the class
+        simpleValue = 10; //This is the starting point of the value, determined here in the constructor
     }
 }
 
-class TestVerySimpleClass
+
+
+
+
+//Clients and end-users who want to access the architect tools
+class TestVerySimpleClass //The class user has to follow the access rules
 {
     static void Main()
     {
-        VerySimple ver; // notice that ver is null
+        //This is called instantiation: creating an object of type class that you are accessing
 
-        ver = new VerySimple();
+        VerySimple ver = new VerySimple();
+
+        //Name of the class you are instantiating from: VerySimple
+        //create a name called "ver" which is the name of the instantiated object
+        //you have to use the key word "new" : instantiation of the object being created
+        //VerySimple() is the call for the constructor (if any)
+
+        var x = ver.simpleValue; //This value was recently made available by adding public to simpleValue above
     }
 }
