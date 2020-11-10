f30f87aeb518135c90107730addd86dea8c94dec
Lars Passic <larspassic@outlook.com>
2020-11-09 18:48:44 -0800

    Working on scratch
    Added gitignore too

--- Scratch/Program.cs
+++ Scratch/Program.cs
@@ -1,34 +1,19 @@
-﻿// Create an array of numbers and find the max and min values of your array
-
-using System;
+﻿using System;
 
 class Program
 {
     static void Main()
     {
+        // This called boxing
+        object box = 15;
+        Console.WriteLine("The value inside the box is {0}", box);
 
-        // start with the given array
-        int[] numbers = new int[] { 0, 2, 5, 100, -1, 4, 8, -5 };
+        // Convert the object box to a string
+        Console.WriteLine("The ToString of the box is {0}", box.ToString());
 
-
-        // define the max and min
-        int smallestNumber = 0;
-        int largestNumber = 0;
-
-        foreach (int x in numbers)
-        {
-            if (x < smallestNumber)
-            {
-                smallestNumber = x;
-            }
-            if (x > largestNumber)
-            {
-                largestNumber = x;
-            }
-        }
-
-        Console.WriteLine("The Minimum value is {0}", smallestNumber);
-        Console.WriteLine("The Maximum value is {0}", largestNumber);
+        // This is called unboxing - converting an object to an integer
+        int number = (int)box;
+        Console.WriteLine("Number is {0}", number);
 
         Console.ReadLine();
     }
