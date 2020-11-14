aa14c324191ea40e639712d1bf5dc1a06bc6c643
Lars Passic <larspassic@outlook.com>
2020-11-14 11:16:38 -0800

    Finished WinForms exercise
    Finished!!

--- Labs/WinProject1/WinForm1.cs
+++ Labs/WinProject1/WinForm1.cs
@@ -13,7 +13,13 @@ class MyForm : Form
         //Change the caption of the application.
         Text = "Hello World";
         BackColor = System.Drawing.Color.Red;
-
+        MinimizeBox = false;
+        //TransparencyKey = System.Drawing.Color.Red;
+        Opacity = .25;
+        //WindowState = FormWindowState.Maximized;
+        WindowState = FormWindowState.Normal;
+        StartPosition = FormStartPosition.CenterScreen;
+        Size = new Size(1280, 720);
     }
 
     static void Main()
