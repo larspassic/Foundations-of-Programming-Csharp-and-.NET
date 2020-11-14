6bd068d20f3ec5c463e637e94227f9a1c2779ab2
Lars Passic <larspassic@outlook.com>
2020-11-14 12:06:23 -0800

    IT WORKS!!
    Added true loop to keep it running

--- Challenges/ReverseAString/Program.cs
+++ Challenges/ReverseAString/Program.cs
@@ -10,45 +10,50 @@ namespace ReverseAString
     {
         static void Main(string[] args)
         {
-            //Provide instructions to the user
-            Console.Write("Please enter a word or number, and the program will reverse it!");
-
-            //Receive input from the user as a string
-            string userInput = Console.ReadLine();
-
-            //Create an array with chars?
-            char[] charArray = new char[userInput.Length];
-
-            //loop to store the chars from the userInput string as array elements
-            int count = 0;
-            foreach (char charElement in userInput)
+            while (true)
             {
-                charArray[count] = userInput[count];
-                count++;
-            }
 
-            //Another loop to reverse the array
-            char[] reverseArray = new char[charArray.Length];
-            count = 0;
-            int reverseCount = charArray.Length;
-            foreach (char charElement in charArray)
-            {
-                reverseArray[count] = charArray[reverseCount];
-                count++;
-                reverseCount--;
-            }
+                //Provide instructions to the user
+                Console.Write("Please enter a word or number, and the program will reverse it!");
 
-            //Put the chars back into a new string
-            string reversedString = "";
-            count = 0;
-            foreach (char charElement in reverseArray)
-            {
-                reversedString = reversedString + reverseArray[count];
-                count++;
-            }
+                //Receive input from the user as a string
+                string userInput = Console.ReadLine();
 
-            //Display the results to the user
-            Console.WriteLine($"{reversedString}");
+                //Create an array with chars?
+                char[] charArray = new char[userInput.Length];
+
+                //loop to store the chars from the userInput string as array elements
+                int count = 0;
+                foreach (char charElement in userInput)
+                {
+                    charArray[count] = userInput[count];
+                    count++;
+                }
+
+                //Another loop to reverse the array
+                char[] reverseArray = new char[charArray.Length];
+                count = 0;
+                int reverseCount = charArray.Length - 1;
+                foreach (char charElement in charArray)
+                {
+                    reverseArray[count] = charArray[reverseCount];
+                    count++;
+                    reverseCount--;
+                }
+
+                //Put the chars back into a new string
+                string reversedString = "";
+                count = 0;
+                foreach (char charElement in reverseArray)
+                {
+                    reversedString = reversedString + reverseArray[count];
+                    count++;
+                }
+
+                //Display the results to the user
+                Console.WriteLine($"{reversedString}");
+                Console.WriteLine();
+            }
         }
     }
 }
