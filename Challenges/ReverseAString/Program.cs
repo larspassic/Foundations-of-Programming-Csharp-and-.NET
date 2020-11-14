9a79a6ee0dbc2a8bdaf5ac7626e392637e014603
Lars Passic <larspassic@outlook.com>
2020-11-14 11:39:56 -0800

    Created ReverseAString challenge
    First commit

---
+++ Challenges/ReverseAString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Provide instructions to the user
+            Console.Write("Please enter a word or number, and the program will reverse it!");
+
+            //Receive input from the user as a string
+            string userInput = Console.ReadLine();
+
+            //Create an array with chars?
+            char[] charArray = new char[];
+        }
+    }
+}
