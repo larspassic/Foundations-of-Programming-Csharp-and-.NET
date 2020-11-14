0e3cd0a209a23c08fbd8b6217146f808a22da5a4
Lars Passic <larspassic@outlook.com>
2020-11-14 10:42:39 -0800

    Created and ran the form program

---
+++ Labs/WinProject1/WinForm1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+class MyForm : Form
+{
+    public MyForm()
+    {
+        //Change the caption of the application.
+        Text = "Hello World";
+    }
+
+    static void Main()
+    {
+        MyForm myForm = new MyForm();
+
+        //Display the form.
+        Application.Run(myForm);
+    }
+}
