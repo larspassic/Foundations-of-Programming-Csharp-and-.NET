ab0edbecb691d96eee84246e2be4c6825530da14
Lars Passic <larspassic@outlook.com>
2020-11-17 08:10:14 -0800

    Created SumDigitsOfInteger
    init

---
+++ Challenges/SumDigitsOfInteger/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumDigitsOfInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                //Ask the user for input
+                Console.Write("Please enter an integer, and the program will sum the digits:");
+
+                //Take the input from the user as a string
+                string strUserInput = Console.ReadLine();
+
+                int intUserInput = int.Parse(strUserInput);
+
+
+            }
+        }
+    }
+}
