a026e3a5b242cb6f45ddc00f5233f92f2887234a
Lars Passic <larspassic@outlook.com>
2020-11-16 20:17:40 -0800

    Base program

--- Labs/ModifyAStructure/Program.cs
+++ Labs/ModifyAStructure/Program.cs
@@ -1,15 +1,93 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ModifyAStructure
+public struct Pet
 {
-    class Program
+    public string Name;
+    public string TypeOfPet;
+}
+
+class Program
+{
+    static void Main()
     {
-        static void Main(string[] args)
+        var numberOfPets = 0;
+        var pets = new Pet[10];
+
+        while (true)
         {
+            Console.Write("A)dd D)elete L)ist pets:");
+            var choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "A":
+                case "a":
+                    {
+                        Console.Write("Name :");
+                        var name = Console.ReadLine();
+
+                        Console.Write("Type of pet :");
+                        var typeOfPet = Console.ReadLine();
+
+                        // TODO: Always add the pet at the end of the array
+
+                        numberOfPets++;
+                        break;
+                    }
+
+                case "D":
+                case "d":
+                    {
+                        if (numberOfPets == 0)
+                        {
+                            Console.WriteLine("No pets");
+                            break;
+                        }
+
+                        for (var index = 0; index < numberOfPets; index++)
+                        {
+                            Console.WriteLine("{0}. {1,-10} {2}", index + 1, pets[index].Name, pets[index].TypeOfPet);
+                        }
+
+                        Console.Write("Which pet to remove (1-{0})", numberOfPets);
+
+                        var petNumberToDelete = Console.ReadLine();
+                        var indexToDelete = int.Parse(petNumberToDelete);
+
+                        // Squish the array from index to the end
+
+                        for (var index = indexToDelete - 1; index < numberOfPets; index++)
+                        {
+                            // TODO: Just copy the pet from the next index into the current index
+                        }
+
+                        // We have one less pet
+                        numberOfPets--;
+
+                        break;
+                    }
+
+                case "L":
+                case "l":
+                    {
+                        if (numberOfPets == 0)
+                        {
+                            Console.WriteLine("No pets");
+                        }
+
+                        for (int index = 0; index < numberOfPets; index++)
+                        {
+                            Console.WriteLine("{0}. {1,-10} {2}", index + 1, pets[index].Name, pets[index].TypeOfPet);
+                        }
+
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Invalid option [{0}]", choice);
+                        break;
+                    }
+            }
         }
     }
 }
