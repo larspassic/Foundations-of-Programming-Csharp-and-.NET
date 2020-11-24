73275ca3c3f713a3e20dee46382c9ecaaba508ef
Lars Passic <larspassic@outlook.com>
2020-11-24 09:57:49 -0800

    Created ConvertStringToLowercase challenge
    init

---
+++ Challenges/ConvertStringToLowercase/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertStringToLowercase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask the user to enter a string
+            Console.Write("Please enter a string, and the program will convert it to lowercase:");
+
+            //Collect input as a string
+            string strUserInput = Console.ReadLine();
+
+            //Loop to put the string into a char array
+
+            //Find out the unicode
+
+            //Loop to subtract 20 from the unicode number and populate a new char array
+
+            //Loop to build a final string from the new char array
+            string strFinalString = "";
+
+            //Output the final string to the user
+            Console.WriteLine($"Your string converted to lowercase is {strFinalString}");
+        }
+    }
+}
