40a0341f6be3177325bc0f8c14d6c2a147ee7256
Lars Passic <larspassic@outlook.com>
2020-11-24 11:54:37 -0800

    Updates

--- Challenges/ConvertStringToLowercase/Program.cs
+++ Challenges/ConvertStringToLowercase/Program.cs
@@ -10,23 +10,26 @@ namespace ConvertStringToLowercase
     {
         static void Main(string[] args)
         {
-            //Ask the user to enter a string
-            Console.Write("Please enter a string, and the program will convert it to lowercase:");
+            while (true)
+            {
+                //Ask the user to enter a string
+                Console.Write("Please enter a string, and the program will convert it to lowercase:");
 
-            //Collect input as a string
-            string strUserInput = Console.ReadLine();
+                //Collect input as a string
+                string strUserInput = Console.ReadLine();
 
-            //Loop to put the string into a char array
+                //Loop to put the string into a char array
 
-            //Find out the unicode
+                //Find out the unicode
 
-            //Loop to subtract 20 from the unicode number and populate a new char array
+                //Loop to subtract 20 from the unicode number, which will result in the lowercase version of the unicode character, and populate a new char array
 
-            //Loop to build a final string from the new char array
-            string strFinalString = "";
+                //Loop to build a final string from the new char array
+                string strFinalString = "";
 
-            //Output the final string to the user
-            Console.WriteLine($"Your string converted to lowercase is {strFinalString}");
+                //Output the final string to the user
+                Console.WriteLine($"Your string converted to lowercase is {strFinalString}");
+            }
         }
     }
 }
