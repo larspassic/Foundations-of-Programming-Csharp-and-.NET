2175880ab63987e200ff9aefbd8a8c83626c7e2f
Lars Passic <larspassic@outlook.com>
2020-11-23 21:48:38 -0800

    Finished challenge Average10Floats
    It works!

--- Challenges/Average10Floats/Program.cs
+++ Challenges/Average10Floats/Program.cs
@@ -18,21 +18,26 @@ namespace Average10Floats
                 //Establish the array to hold the numbers
                 float[] floatArray = new float[10];
 
+                //Define an integer to hold how many elements we have in the array
+                int numberOfFloats = 0;
+
                 //Since we are collecting up to 10 numbers, use a loop to gather input
                 for (int i = 0; i < 10; i++)
                 {
-                    Console.WriteLine("Please enter a number (or just press enter to average the current set of numbers:");
+                    Console.Write("Please enter a number (or just press enter to average the current set of numbers):");
                     
                     //Take user input as string
                     string userInputString = Console.ReadLine();
-                    float userInputFloat = float.Parse(userInputString);
                     
                     if (userInputString == "") //Break out of the loop if the user hit enter without typing a number
                     {
                         break;
                     }
                     
+                    float userInputFloat = float.Parse(userInputString);
+                    
                     floatArray[i] = userInputFloat;
+                    numberOfFloats++;
                 }
 
                 //Loop to add up all of the numbers in the floatArray
@@ -43,7 +48,7 @@ namespace Average10Floats
                     sumFloats += floatArray[i];
                 }
 
-                float averageFloats = sumFloats / floatArray.Length;
+                float averageFloats = sumFloats / numberOfFloats;
 
                 //Send output to the user
                 Console.WriteLine($"The average of the numbers you entered was {averageFloats}");
