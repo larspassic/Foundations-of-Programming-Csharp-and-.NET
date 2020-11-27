6d804cb64b0bcf3464baf217f67c27429d1b00b9
Lars Passic <larspassic@outlook.com>
2020-11-27 14:37:04 -0800

    Updates
    Not working yet.

--- Challenges/ConvertStringToLowercase/Program.cs
+++ Challenges/ConvertStringToLowercase/Program.cs
@@ -20,23 +20,28 @@ namespace ConvertStringToLowercase
 
                 //Loop to put the chars into an char array
                 int[] intArray = new int[strUserInput.Length];
-
+                decimal[] decArray = new decimal[intArray.Length];
                 //Hopefully converting to an int will result in unicode value
                 for (int i=0; i < strUserInput.Length; i++)
                 {
                     intArray[i] = strUserInput[i];
+                    decArray[i] = strUserInput[i];
                 }
 
 
                 //Loop to subtract 20 from the unicode number, which will result in the lowercase version of the unicode character, and populate a new char array
                 char[] charArray = new char[intArray.Length];
+                
                 for (int i=0; i < intArray.Length; i++)
                 {
-                    charArray[i] = (char)(intArray[i]-20);
+
+                    charArray[i] = (char)(intArray[i]);
+                    
                 }
 
                 //Loop to build a final string from the new char array
-                //string strFinalString = charArray.ToString(); //Cheating
+                string strFinalString = new String(charArray); //Cheating? maybe not...
+                /*
                 string strFinalString = "";
 
                 for (int i = 0; i < charArray.Length; i++)
@@ -45,7 +50,7 @@ namespace ConvertStringToLowercase
                     strFinalString = strFinalString + charArray[i];
 
                 }
-                
+                */
 
                 //Output the final string to the user
                 Console.WriteLine($"Your string converted to lowercase is {strFinalString}");
