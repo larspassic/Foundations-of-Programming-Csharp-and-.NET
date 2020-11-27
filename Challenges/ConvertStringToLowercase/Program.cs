77a8e5d537390ba0529cc2a14f1ebb78b05480eb
Lars Passic <larspassic@outlook.com>
2020-11-27 15:37:23 -0800

    Finally got it working with +32 offset to convert to lower case
    Checked out my classmate's solution!

--- Challenges/ConvertStringToLowercase/Program.cs
+++ Challenges/ConvertStringToLowercase/Program.cs
@@ -13,47 +13,42 @@ namespace ConvertStringToLowercase
             while (true)
             {
                 //Ask the user to enter a string
-                Console.Write("Please enter a string, and the program will convert it to lowercase:");
+                Console.Write("Please enter a string, and the program will convert any UPPERCASE LETTERS to lowercase:");
 
                 //Collect input as a string
                 string strUserInput = Console.ReadLine();
 
                 //Loop to put the chars into an char array
                 int[] intArray = new int[strUserInput.Length];
-                decimal[] decArray = new decimal[intArray.Length];
+
                 //Hopefully converting to an int will result in unicode value
                 for (int i=0; i < strUserInput.Length; i++)
                 {
                     intArray[i] = strUserInput[i];
-                    decArray[i] = strUserInput[i];
                 }
 
 
-                //Loop to subtract 20 from the unicode number, which will result in the lowercase version of the unicode character, and populate a new char array
+                //Loop to perform some arithmetic on the unicode number, which will result in the lowercase version of the unicode character, and populate a new char array
                 char[] charArray = new char[intArray.Length];
                 
                 for (int i=0; i < intArray.Length; i++)
                 {
-
-                    charArray[i] = (char)(intArray[i]);
-                    
+                    if (intArray[i] > 64 && intArray[i] < 91) //Looking for 65 through 90 which is the range of uppercase characters
+                    {
+                        charArray[i] = (char)(intArray[i] + 32); //Uppercase are exactly 32 less than lowercase characters, add 32 to get lowercase
+                    }
+                    else
+                    {
+                        charArray[i] = (char)(intArray[i]);
+                    }
                 }
 
                 //Loop to build a final string from the new char array
                 string strFinalString = new String(charArray); //Cheating? maybe not...
-                /*
-                string strFinalString = "";
-
-                for (int i = 0; i < charArray.Length; i++)
-                {
-
-                    strFinalString = strFinalString + charArray[i];
-
-                }
-                */
 
                 //Output the final string to the user
-                Console.WriteLine($"Your string converted to lowercase is {strFinalString}");
+                Console.WriteLine($"Your string with uppercase characters converted to lowercase is {strFinalString}");
+                Console.WriteLine();
             }
         }
     }
