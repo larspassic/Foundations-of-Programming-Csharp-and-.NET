c01353b48367807c8fea18467568cc22741ee7f7
Lars Passic <larspassic@outlook.com>
2020-11-30 18:47:51 -0800

    Created lab Lists

---
+++ Labs/Lists/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+struct Contact
+{
+    public String Name;
+    public String PhoneNumber;
+}
+
+class Program
+{
+    static void Main()
+    {
+        // TODO: Create a list of Contact
+        var contacts;
+
+        // TODO: Add contacts to the list
+        contacts.Add();
+        contacts.Add();
+
+        // TODO: Display the Name and PhoneNumber of all the contacts
+        foreach ( )
+        {
+        }
+
+        Console.ReadLine();
+    }
+}
