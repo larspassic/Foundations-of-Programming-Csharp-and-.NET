c664739599824e31e55c7da31ba6b9968649bd02
Lars Passic <43658463+larspassic@users.noreply.github.com>
2020-12-02 14:11:29 -0800

    Update Scratch pad

--- Scratch/Program.cs
+++ Scratch/Program.cs
@@ -1,40 +1,51 @@
-﻿// File: VerySimple.cs
-using System;
+﻿using System;
 
-
-
-//Back-end architect developers who create tools and classes for us to use
-class VerySimple //This is a class
+class Point
 {
-    public int simpleValue; //Field - added the "public" to make it available in the test below
+    private int X;
+    private int Y;
 
-    public VerySimple() //Constructor
+    // public Constructor
+    public Point(int x, int y)
     {
-        //initializers : superintendents of the class
-        simpleValue = 10; //This is the starting point of the value, determined here in the constructor
+        X = x;
+        Y = y;
+    }
 
+    public void IncrementXBy(int x)
+    {
+        X = X + x;
+    }
+    public void IncrementXBy(long x)
+    {
+        X = X + (int)x;
+    }
+    public void IncrementXBy(double x)
+    {
+        X = X + (int)x;
     }
 }
 
-
-
-
-
-//Clients and end-users who want to access the architect tools
-class TestVerySimpleClass //The class user has to follow the access rules
+class Program
 {
     static void Main()
     {
-        //This is called instantiation: creating an object of type class that you are accessing
-
-        VerySimple ver = new VerySimple();
-
-        //Name of the class you are instantiating from: VerySimple
-        //create a name called "ver" which is the name of the instantiated object
-        //you have to use the key word "new" : instantiation of the object being created
-        //VerySimple() is the call for the constructor (if any)
-
-        var x = ver.simpleValue; //This value was recently made available by adding public to simpleValue above
         
+        
+        var pt = new Point(10, 20);
+        pt.
+        // Call int version because
+        // 2 is an integer
+        pt.IncrementXBy(2);
+
+        // Call long version because
+        // 2 is cast as a long integer
+        pt.IncrementXBy((long)2);
+
+        // Call double version because
+        // 3.4 is a double
+        pt.IncrementXBy(3.4);
+
+        Console.ReadLine();
     }
 }
