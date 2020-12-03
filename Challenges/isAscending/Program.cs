29a53d677207f3c2d8e3b9ee472867a8cdd92b39
Lars Passic <larspassic@outlook.com>
2020-12-03 15:45:01 -0800

    Created isAscending challenge

---
+++ Challenges/isAscending/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = new int[] { 15, 22, 71 };
+
+        // What should the default value be?
+        bool isAscending;
+
+        loop through the numbers array
+        {
+            // How do you know if it is ascending?
+        }
+
+        Console.WriteLine("The array is ascending: {0}", isAscending);
+
+        Console.ReadLine();
+    }
+}
