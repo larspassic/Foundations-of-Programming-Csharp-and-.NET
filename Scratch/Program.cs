21aa7ae93c3f5415b0dd664ed6b3b6a22e46c291
Lars Passic <larspassic@outlook.com>
2020-12-07 18:19:10 -0800

    Update scratch with new testing

--- Scratch/Program.cs
+++ Scratch/Program.cs
@@ -1,51 +1,60 @@
 ﻿using System;
 
-class Point
+class Vehicle //Base class or parent class
 {
-    private int X;
-    private int Y;
+    public string Name;
+    public int Passengers;
+    public int Speed;
 
-    // public Constructor
-    public Point(int x, int y)
+    public Vehicle(string name, int passengers, int speed)
     {
-        X = x;
-        Y = y;
-    }
-
-    public void IncrementXBy(int x)
-    {
-        X = X + x;
-    }
-    public void IncrementXBy(long x)
-    {
-        X = X + (int)x;
-    }
-    public void IncrementXBy(double x)
-    {
-        X = X + (int)x;
+        Name = name;
+        Passengers = passengers;
+        Speed = speed;
     }
 }
 
-class Program
+class Automobile : Vehicle //The child class - inherits from base class vehicle
 {
+    public int Windows;
+
+    public Automobile(string name, int passengers, int speed, int windows)
+       : base(name, passengers, speed) //Colon base - this indicates which fields are inherited from the parent class
+    {
+        Windows = windows;
+    }
+}
+
+class Motorcycle : Vehicle //Another child class - inherits from base class vehicle
+{
+    public int Seats;
+
+    public Motorcycle(string name, int passengers, int speed, int seats)
+        : base(name, passengers, speed)
+    {
+        Seats = seats;
+    }
+}
+
+
+class Program //End user or client that makes use of the vehicle family
+{
+    // Notice that this take a base class Vehicle object
+    // and not a specific child class
+    public static void PrintInfo(Vehicle v)
+    {
+        Console.WriteLine("Name is {0}", v.Name);
+        Console.WriteLine("Passengers is {0}", v.Passengers);
+    }
+
     static void Main()
     {
-        
-        
-        var pt = new Point(10, 20);
-        pt.
-        // Call int version because
-        // 2 is an integer
-        pt.IncrementXBy(2);
+        var myVehicle = new Vehicle("My Vehicle", 0, 100);
+        var myHondaCivic = new Automobile("Honda Civic", 2, 120, 2);
+        var myHarley = new Motorcycle("Harley Low Rider", 1, 100, 1);
 
-        // Call long version because
-        // 2 is cast as a long integer
-        pt.IncrementXBy((long)2);
-
-        // Call double version because
-        // 3.4 is a double
-        pt.IncrementXBy(3.4);
-
-        Console.ReadLine();
+        PrintInfo(myVehicle);
+        PrintInfo(myHondaCivic);
+        PrintInfo(myHarley);
     }
 }
