721a14a90c41cf1fc8b6d794071be8c897f84249
Lars Passic <larspassic@outlook.com>
2020-12-09 18:30:14 -0800

    Updated cases with lowercase equivalents

--- Homework/Homework6-FinalProject/Program.cs
+++ Homework/Homework6-FinalProject/Program.cs
@@ -6,6 +6,17 @@ using System.Threading.Tasks;
 
 namespace Homework6_FinalProject
 {
+    
+    struct Item
+    {
+        string itemNumber;
+        string description;
+        float externalCost;
+        float internalCost;
+        int quantityOnHand;
+        float itemValue;
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -16,26 +27,39 @@ namespace Homework6_FinalProject
                 //Introduction instructions
                 Console.WriteLine("Please make a selection:\nA)dd an item\nC)hange an item\nD)elete\nL)ist all items\nQuit");
 
+                //Take input from the user
                 string strUserInput = Console.ReadLine();
                 
+                //Main switch statement
                 switch (strUserInput)
                 {
                     case "A":
+                    case "a":
+                        Console.WriteLine("Case A");
                         break;
                     
                     case "C":
+                    case "c":
+                        Console.WriteLine("Case C");
                         break;
                     
                     case "D":
+                    case "d":
+                        Console.WriteLine("Case D");
                         break;
                     
                     case "L":
+                    case "l":
+                        Console.WriteLine("Case L");
                         break;
                     
                     case "Q":
+                    case "q":
+                        Console.WriteLine("Case Q");
                         break;
                     
                     default:
+                        Console.WriteLine("Default case");
                         break;
                 }
                 
