2238f954b9bb7f8fd939f9b8b23117f06b5a8879
Lars Passic <larspassic@outlook.com>
2020-12-14 19:20:55 -0800

    Created TryCatch lab

---
+++ Labs/TryCatch/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            int x = 1;
+
+            x = x / (x - 1);
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("{0} {1}", ex.Message, ex.StackTrace);
+        }
+
+        Console.ReadLine();
+    }
+}
