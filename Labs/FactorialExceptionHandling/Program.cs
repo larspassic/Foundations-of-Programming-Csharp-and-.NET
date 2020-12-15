265c5280ff8e3d78e57236b5c922ab1083d829b7
Lars Passic <larspassic@outlook.com>
2020-12-14 19:10:26 -0800

    Exception Handling

---
+++ Labs/FactorialExceptionHandling/Program.cs
@@ -0,0 +1,19 @@
+﻿// Factorial.cs
+using System;
+
+class Factorial
+{
+    static void Main()
+    {
+        long nFactorial = 1;
+        long nComputeTo = 200; // Calc 200 factorial
+
+        for (long nCurDigit = 1; nCurDigit <= nComputeTo; nCurDigit++)
+        {
+            nFactorial = nFactorial * nCurDigit;
+        }
+
+        Console.WriteLine("{0}! is {1}", nComputeTo, nFactorial);
+        Console.ReadLine();
+    }
+}
