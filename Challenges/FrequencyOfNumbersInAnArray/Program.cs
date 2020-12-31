9ab33402f51aa2b6af423a18d827ae3e70b16b22
Lars Passic <43658463+larspassic@users.noreply.github.com>
2020-12-31 10:24:20 -0800

    Created FrequencyOfNumbersInAnArray challenge

---
+++ Challenges/FrequencyOfNumbersInAnArray/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FrequencyOfNumbersInAnArray
+{
+
+    class Stack
+    {
+        int[] stackInt = new int[10];
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+                                
+                int[] stack = new int[10];
+
+                while (true)
+                {
+                    
+                    
+                    //Ask the user to build an array of numbers
+                    Console.WriteLine("Enter an integer (whole) number between 0 and 100, or press enter to continue:");
+
+                    string userInput = Console.ReadLine();
+
+                    if (userInput == null)
+                    {
+                        break;
+                    }
+
+                    int userInputInt = int.Parse(userInput);
+
+
+                }
+
+
+            }
+        }
+    }
+}
